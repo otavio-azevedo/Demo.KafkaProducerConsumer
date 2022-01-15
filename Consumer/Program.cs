ac3d4cad4a0b96fee7b0d439cfa20d76a571d6be
otavio-azevedo <otavioazevedo7@gmail.com>
2022-01-15 16:31:11 -0300

    Check in project

---
+++ Consumer/Program.cs
@@ -0,0 +1,75 @@
+﻿using Confluent.Kafka;
+using Serilog;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+            
+            logger.Information("Testando o consumo de mensagens com Kafka");
+
+            if (args.Length != 2)
+            {
+                logger.Error(
+                    "Enter at least 2 parameters:\n" +
+                    "1) IP:port hosting Kafka\n" +
+                    "2) The Topic to be used in the consumption of messages\n\n" +
+                    "Example: dotnet run \"localhost:9092\" \"topic-example\"");
+                return;
+            }
+
+            string bootstrapServers = args[0];
+            string nomeTopic = args[1];
+
+            logger.Information($"BootstrapServers = {bootstrapServers}");
+            logger.Information($"Topic = {nomeTopic}");
+
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = bootstrapServers,
+                GroupId = $"{nomeTopic}-group-0",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
+            try
+            {
+                using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+                {
+                    consumer.Subscribe(nomeTopic);
+
+                    try
+                    {
+                        while (true)
+                        {
+                            var cr = consumer.Consume(cts.Token);
+                            logger.Information(
+                                $"Message read: {cr.Message.Value}");
+                        }
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        consumer.Close();
+                        logger.Warning("Canceled execution of Consumer...");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error($"Exception: {ex.GetType().FullName} | " +
+                             $"Message: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,64 @@
+﻿using Confluent.Kafka;
+using Serilog;
+
+namespace Producer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            logger.Information("Testing sending messages with Kafka");
+
+            if (args.Length < 3)
+            {
+                logger.Error(
+                    "Enter at least 3 parameters:\n" +
+                    "1) IP:port hosting Kafka\n" +
+                    "2) The Topic that will receive the messages\n" +
+                    "3) The messages to be sent to a Topic in Kafka\n\n" +
+                    "Example: dotnet run \"localhost:9092\" \"topic-example\" \"Hi\" \"How are you?\"");
+                return;
+            }
+
+            string bootstrapServers = args[0];
+            string nomeTopic = args[1];
+
+            logger.Information($"BootstrapServers = {bootstrapServers}");
+            logger.Information($"Topic = {nomeTopic}");
+
+            try
+            {
+                var config = new ProducerConfig
+                {
+                    BootstrapServers = bootstrapServers
+                };
+
+                using (var producer = new ProducerBuilder<Null, string>(config).Build())
+                {
+                    for (int i = 2; i < args.Length; i++)
+                    {
+                        var result = await producer.ProduceAsync(
+                            nomeTopic,
+                            new Message<Null, string>
+                            { Value = args[i] });
+
+                        logger.Information(
+                            $"Message: {args[i]} | " +
+                            $"Status: { result.Status.ToString()}");
+                    }
+                }
+
+                logger.Information("Finished sending messages");
+            }
+            catch (Exception ex)
+            {
+                logger.Error($"Exception: {ex.GetType().FullName} | " +
+                             $"Message: {ex.Message}");
+            }
+        }
+    }
+}
